3899e69bc79f85a0a098f18b6c645eae81a8e589
DamienGidon <dgidon@intechinfo.fr>
2017-11-15 10:14:34 +0100

    add create channel

--- App/src/app/components/menu/menu.ts
+++ App/src/app/components/menu/menu.ts
@@ -1,13 +1,55 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input,OnInit, EventEmitter } from '@angular/core';
 import { Channel } from 'models';
+import { ChannelService, PostSocketService } from '../../services/index';
+import { Router } from '@angular/router';
+//import { AuthenticationService } from '../../services/index';
 
-/**
- * Side menu that allow to naviaget inside the differents channels
- */
+
 @Component({
     selector: 'menu',
     templateUrl: 'menu.html'
 })
-export class MenuComponent {
-    @Input() channels: Channel[] = [];
+export class MenuComponent implements OnInit { 
+    model = new Channel();
+
+    constructor(private channelService: ChannelService,
+            private router : Router,
+            private postSocket: PostSocketService,
+
+    ) {
+        this.postSocket.onNewChannel(this.addChannel)
+    }
+    async ngOnInit() { 
+        this.channels = (await this.channelService.getAll()).sort(this.sortChannels);
+        if(this.channels.length>0)
+        this.router.navigateByUrl("/channel/"+this.channels[0].id);
+    }
+    async save() {
+         if(this.channels.findIndex(x=>x.name===this.model.name)===-1){
+            await this.channelService.add(this.model.name);
+            this.channels = await this.channelService.getAll();
+            this.channels.sort(this.sortChannels);
+         }
+    }
+    callParent(chanName: string){
+        this.model.name=chanName;
+        this.save();
+    }
+    sortChannels(n1: Channel,n2: Channel){
+    if (n1.name > n2.name) {
+        return 1;
+    }
+    if (n1.name < n2.name) {
+        return -1;
+    }
+    return 0;
+    }
+   addChannel = (chan:Channel) => {
+        this.channels.unshift(chan);
+    }
+    @Input() channels: Channel[]; 
+
+
+    
+   
 }
